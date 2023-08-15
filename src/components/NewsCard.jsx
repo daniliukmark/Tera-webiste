18a2e3d58184b294567d5152fc08acba4ee05669
daniliukmark <daniliukmark@gmail.com>
2023-08-15 16:27:59 +0300

    ss

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
 import styles from "src/styles/NewsCard.module.css";
-function NewsCard() {
+function NewsCard({ postPath }) {
   return (
     <>
-      <div className={styles["news-card-wrap"]}></div>
+      <div className={styles["news-card-wrap"]}>
+        <div className={styles["card-ceontent"]}>
+          <h1 className={styles["header"]}>
+            <i>Pasiruosimas sezuonui pradetas</i>
+          </h1>
+          <p className={styles["content-snippet"]}>
+            Prieš pradedant antrojo rato mūšius PAFF - ŠAFF...
+          </p>
+          <Link className={styles["link"]} to={{ pathname: postPath }}>
+            <b>Suzinoti Daugiau...</b>
+          </Link>
+        </div>
+        <div className={styles["header-content"]}></div>
+      </div>
     </>
   );
 }
